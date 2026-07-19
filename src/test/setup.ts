369bda2789cddaeb2ae680119d90919344b31e15
rongwei.liu <568264317@qq.com>
2026-07-19 12:45:33 +0800

    test: clean up react trees after each vitest case

---
+++ src/test/setup.ts
@@ -0,0 +1,13 @@
+/**
+ * Copyright (c) 2026 OpenBKN
+ * SPDX-License-Identifier: LicenseRef-OpenBKN
+ * Licensed under the OpenBKN License, a modified Apache 2.0 with Additional
+ * Conditions. See LICENSE for the full text.
+ */
+
+import { cleanup } from "@testing-library/react";
+import { afterEach } from "vitest";
+
+afterEach(() => {
+  cleanup();
+});
