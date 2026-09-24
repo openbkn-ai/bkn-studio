fbc3c86cc0c021fc6e23863fb8b54d69b9e74b34
root <18717890337@139.com>
2026-09-24 15:10:54 +0800

    chore(studio): add permission request license headers

--- src/modules/account/components/PermissionRequestsPanel.tsx
+++ src/modules/account/components/PermissionRequestsPanel.tsx
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2026 OpenBKN
+ * SPDX-License-Identifier: LicenseRef-OpenBKN
+ * Licensed under the OpenBKN License, a modified Apache 2.0 with Additional
+ * Conditions. See LICENSE for the full text.
+ */
+
 import { Alert, Avatar, Button, Divider, Drawer, Form, Input, Space, Table, Tabs, Tag, Timeline, Tooltip } from "antd";
 import { useEffect, useState, type ReactNode } from "react";
 import { Trans, useTranslation } from "react-i18next";
